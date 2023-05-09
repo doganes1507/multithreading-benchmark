c66165760320e6ba119e1f11687a66e6422f512f
doganes1507 <d.oganes1507@gmail.com>
2023-05-09 14:02:40 +0300

    Add new multithreading benchmark: searching prime numbers in the specified range with following files:
    Benchmark.cs with methods RunSingleTest and RunMultipleTests
    
    Primes.cs with IsPrime and ParallelFindPrimes
    
    All of the methods comes without implementation but with input validation and documentation

---
+++ MultithreadingBenchmark.FindPrimes/Benchmark.cs
@@ -0,0 +1,84 @@
+﻿namespace MultithreadingBenchmark.FindPrimes;
+
+public static class Benchmark
+{
+    /// <summary>
+    /// Runs a single test to measure the time it takes to find prime numbers in the specified range using the specified number of threads.
+    /// </summary>
+    /// <param name="numberOfThreads">The number of threads to use for the search.</param>
+    /// <param name="startRange">The start of the range to search for prime numbers.</param>
+    /// <param name="endRange">The end of the range to search for prime numbers.</param>
+    /// <returns>The elapsed time in milliseconds.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when the number of threads is less than or equal to 0 or the start range is less than 2.</exception>
+    /// <exception cref="ArgumentException">Thrown when the endRange is less than the startRange.</exception>
+    public static long RunSingleTest(int numberOfThreads, int startRange, int endRange)
+    {
+        #region InputValidation
+
+        if (numberOfThreads <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(numberOfThreads), "The number of threads must be positive.");
+        }
+
+        if (startRange < 2)
+        {
+            throw new ArgumentOutOfRangeException(nameof(startRange), "The start range must be greater than 1.");
+        }
+
+        if (endRange < startRange)
+        {
+            throw new ArgumentException("The end range must be greater than or equal to the start range.");
+        }
+
+        #endregion
+        
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Runs multiple tests to measure the time it takes to find prime numbers in the specified range using different numbers of threads.
+    /// </summary>
+    /// <param name="numbersOfThreads">List of integers representing the number of threads to use for each test.</param>
+    /// <param name="startRange">The start of the range to search for prime numbers.</param>
+    /// <param name="endRange">The end of the range to search for prime numbers.</param>
+    /// <returns>List of time measurements in milliseconds for each test.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when the input list of thread numbers is null.</exception>
+    /// <exception cref="ArgumentException">Thrown when the input list of thread numbers is empty or the end range is less than the start range.</exception>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when any of the input numbers of threads is less than 1, or the start range is less than 2.</exception>
+    public static List<long> RunMultipleTests(List<int> numbersOfThreads, int startRange, int endRange)
+    {
+        #region InputValidation
+
+        if (numbersOfThreads == null)
+        {
+            throw new ArgumentNullException(nameof(numbersOfThreads));
+        }
+        
+        if (numbersOfThreads.Count == 0)
+        {
+            throw new ArgumentException("The list of thread numbers must contain at least one element.");
+        }
+        
+        foreach (var numberOfThreads in numbersOfThreads)
+        {
+            if (numberOfThreads < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(numberOfThreads), "The number of threads must be positive");
+            }
+        }
+        
+        if (startRange < 2)
+        {
+            throw new ArgumentOutOfRangeException(nameof(startRange), "The start range must be greater than 1.");
+        }
+
+        if (endRange < startRange)
+        {
+            throw new ArgumentException("The end range must be greater than or equal to the start range.");
+        }
+
+        #endregion
+        
+        throw new NotImplementedException();
+    }
+}
---
+++ MultithreadingBenchmark.FindPrimes/Primes.cs
@@ -0,0 +1,56 @@
+﻿namespace MultithreadingBenchmark.FindPrimes;
+
+public static class Primes
+{
+    /// <summary>
+    /// Checks if the given number is a prime number.
+    /// </summary>
+    /// <param name="number">The number to check.</param>
+    /// <returns>True if the number is prime, false otherwise.</returns>
+    /// <exception cref="ArgumentException">Thrown when the number is less than 2.</exception>
+    public static bool IsPrime(int number)
+    {
+        #region InputValidation
+
+        if (number < 2)
+        {
+            throw new ArgumentException("The number must be greater than 1");
+        }
+
+        #endregion
+        
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Finds all prime numbers in the given range in parallel using multiple threads.
+    /// </summary>
+    /// <param name="numberOfThreads">The number of threads to use for the parallel search.</param>
+    /// <param name="startRange">The lower bound of the range to search for prime numbers.</param>
+    /// <param name="endRange">The upper bound of the range to search for prime numbers.</param>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when the number of threads is less than or equal to 0 or the start range is less than 2</exception>
+    /// <exception cref="ArgumentException">Thrown when the endRange is lower than startRange</exception>
+    public static void ParallelFindPrimes(int numberOfThreads, int startRange, int endRange)
+    {
+        #region InputValidation
+
+        if (numberOfThreads <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(numberOfThreads), "The number of threads must be positive.");
+        }
+
+        if (startRange < 2)
+        {
+            throw new ArgumentOutOfRangeException(nameof(startRange), "The start range must be greater than 1.");
+        }
+
+        if (endRange < startRange)
+        {
+            throw new ArgumentException("The end range must be greater than or equal to the start range.");
+        }
+
+        #endregion
+        
+        throw new NotImplementedException();
+    }
+}
